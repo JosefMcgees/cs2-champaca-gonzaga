b6ba75ef1e784e88871abc546f51d453b2d50de2
JosefMcgees <gonzagajp30@wvc.pshs.edu.ph>
2026-01-20 14:57:50 +0800

    yes i already added cool data but it didnt update

---
+++ gonzaga-data.py
@@ -0,0 +1,4 @@
+sleep = [8.07, 12.58, 7.7, 8, 8.33, 7.58, 7.8] #hours slept each day
+screen_time = [8.55, 10.41, 9.43, 6.82, 6.3, 8.07, 6.98] #screen time in hours
+steps = [7421, 8656, 6287, 8794, 7619, 8959, 6372] #total steps per day
+water = [11, 12, 14, 10, 10, 11, 9] #glasses 
